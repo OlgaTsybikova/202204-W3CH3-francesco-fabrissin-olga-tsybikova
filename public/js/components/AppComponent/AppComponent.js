57a2074b92030a4bb58e93b1eb769c9ad93317cf
Olga Tsybikova <tsy2701@gmail.com>
2022-04-20 21:20:18 +0200

    created component info

--- public/js/components/AppComponent/AppComponent.js
+++ public/js/components/AppComponent/AppComponent.js
@@ -17,8 +17,6 @@ class AppComponent extends Component {
           <h2 class="section-title">Series list</h2>
           <section class="series-pending">
             <h3 class="subsection-title">Pending series</h3>
-            <p class="info">You have 4 series pending to watch</p>
-            <!--<p class="info">Congrats! You've watched all your series</p>-->
             <ul class="series-list">
               <li class="serie">
                 <img
--- public/js/components/InfoComponent/InfoComponent.js
+++ public/js/components/InfoComponent/InfoComponent.js
@@ -1,1 +1,20 @@
+import Component from "../Component/Component.js";
 
+class InfoComponent extends Component {
+  text;
+  counter;
+
+  constructor(parentElement, text) {
+    super(parentElement, "p", "info");
+    this.element.textContent = text;
+    this.render();
+  }
+
+  render() {
+    this.element.innerHTML = `
+    <p class="info">You have 4 series pending to watch</p>
+    <!--<p class="info">Congrats! You've watched all your series</p>-->
+    `;
+  }
+}
+export default InfoComponent;
