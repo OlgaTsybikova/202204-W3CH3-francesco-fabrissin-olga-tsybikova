771043857802f99b3fea9326fc3e5c0dcb8c1bc7
Olga Tsybikova <tsy2701@gmail.com>
2022-04-20 18:27:58 +0200

    commit initial

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "import/extensions": "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+  },
+};
