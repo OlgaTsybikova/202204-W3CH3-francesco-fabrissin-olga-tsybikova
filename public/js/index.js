c2aff936e1f7179224ecfc06b2df00a2af65d696
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-04-21 13:19:43 +0200

    refactured stars creating process, added to appComponent

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,3 @@ import StarComponent from "./components/StarComponent/StarComponent.js";
 const body = document.querySelector("body");
 
 new AppComponent(body);
-
-const starsContainers = document.querySelectorAll(".serie");
-
-starsContainers.forEach(
-  (container) => new StarComponent(container, () => "hola")
-);
