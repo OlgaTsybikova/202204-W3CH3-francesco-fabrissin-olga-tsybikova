817e76cc64ad6e11524f96a1d1b046bc740b3a7d
Olga Tsybikova <tsy2701@gmail.com>
2022-04-21 13:50:52 +0200

    added info text to the production

--- public/js/components/AppComponent/AppComponent.js
+++ public/js/components/AppComponent/AppComponent.js
@@ -1,6 +1,7 @@
 import series from "../../series.js";
 import Component from "../Component/Component.js";
 import SerieComponent from "../SerieComponent/SerieComponent.js";
+import InfoComponent from "../InfoComponent/InfoComponent.js";
 
 class AppComponent extends Component {
   constructor(parentElement) {
@@ -19,17 +20,29 @@ class AppComponent extends Component {
           <h2 class="section-title">Series list</h2>
           <section class="series-pending">
           <h3 class="subsection-title">Pending series</h3>
+          <section class="info-container info-container--pending"></section>
           <ul class="series-list">
           </ul>
           </section>
           <section class="series-watched">
             <h3 class="subsection-title">Watched series</h3>
+            <section class="info-container info-container--watched"></section>
             <ul class="series-list">
           </ul>
           </section>
         </section>
       </main>
     `;
+    const infoPendingContainer = document.querySelector(
+      ".info-container--pending"
+    );
+    const infoWatchedContainer = document.querySelector(
+      ".info-container--watched"
+    );
+
+    new InfoComponent(infoPendingContainer);
+
+    new InfoComponent(infoWatchedContainer);
 
     const pendingSeriesContainer = document.querySelector(
       ".series-pending .series-list"
--- public/js/components/InfoComponent/InfoComponent.js
+++ public/js/components/InfoComponent/InfoComponent.js
@@ -3,18 +3,13 @@ import Component from "../Component/Component.js";
 class InfoComponent extends Component {
   text;
 
-  constructor(parentElement, text) {
+  constructor(parentElement) {
     super(parentElement, "p", "info");
-    this.element.textContent = text;
-
     this.render();
   }
 
   render() {
-    this.element.innerHTML = `
-    <p class="info">You have 4 series pending to watch</p>
-    <!--<p class="info">Congrats! You've watched all your series</p>-->
-    `;
+    this.element.textContent = "You have 4 series pending to watch";
   }
 }
 export default InfoComponent;
